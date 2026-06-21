762651195345f5eca83f33e9d7c4b829251b9190
s-dsf <s.desferet@gmail.com>
2026-06-21 19:22:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'thailand-2026-v27';
+const CACHE = 'thailand-2026-v28';
 const ASSETS = ['/', '/index.html', '/voyage.html', '/docx.umd.js'];
 
 self.addEventListener('install', e => {
